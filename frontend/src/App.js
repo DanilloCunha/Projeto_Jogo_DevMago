ef4e74ea864a5c0c533a3c061f8283dece4fc16f
Danillo Cunha <107063327+DanilloCunha@users.noreply.github.com>
2024-10-21 20:16:14 -0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css"; // Estilos da sua aplicação
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo ao Des Game Dev!</h1>
+        <p>Este é o seu jogo de programação.</p>
+        <p>
+          Edite <code>src/App.js</code> e salve para recarregar.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
